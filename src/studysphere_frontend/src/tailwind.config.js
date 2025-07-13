55d2a4efdd9cda0aeff9dfa12b064669f2f49545
Neil <nagasainanduri@gmail.com>
2025-07-13 10:52:59 +0530

    tailwind ui

--- src/studysphere_frontend/src/tailwind.config.js
+++ src/studysphere_frontend/src/tailwind.config.js
@@ -6,3 +6,17 @@ export default {
   },
   plugins: []
 }
+module.exports = {
+  theme: {
+    extend: {
+      boxShadow:{
+        glass: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+      },
+      backdropBlur: {
+        xs: '2px',
+        xxl : '30px',
+      },
+    },
+  },
+  plugins: [],
+};
